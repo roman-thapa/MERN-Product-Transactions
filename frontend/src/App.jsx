f34b0b5d3935199bc43bd827d994e43e38ac4070
roman <51534566+roman-thapa@users.noreply.github.com>
2024-07-08 12:03:19 +0530

    Remove unnecessary code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import TransactionsTable from './components/TransactionsTable';
 import Statistics from './components/Statistics';
 import BarChart from './components/BarChart';
